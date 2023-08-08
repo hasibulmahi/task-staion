fcd79f8cfac0b674a5cd44e3876462ee40723b61
Hasibul Mahi <hasibulmahi@gmail.com>
2023-08-08 22:48:10 +0600

    header update
    todo header size

--- src/NewTodoForm.jsx
+++ src/NewTodoForm.jsx
@@ -1,24 +1,32 @@
-import { useState } from "react"
-export function NewTodoForm({onFormSubmit}) {
-    const [newItem, setNewItem] = useState("")
-    
-    function handleSubmit(e) {
-        e.preventDefault()
-        if (newItem === "") return
-        onFormSubmit(newItem)
-        setNewItem("")
-      
-      }
+import { useState } from "react";
+export function NewTodoForm({ onFormSubmit }) {
+  const [newItem, setNewItem] = useState("");
 
-    return (
-        
-        <form onSubmit={handleSubmit} className="new-item-form">
-    <div className="form-row">
-      <label htmlFor="item">New Item</label>
-      <input value={newItem} onChange={e => setNewItem(e.target.value)}
-       type="text" id="item" />
-    </div>
-    <button className="btn">Add</button>
-  </form>
-    )
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (newItem === "") return;
+    onFormSubmit(newItem);
+    setNewItem("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="new-item-form">
+      <div className="form-row">
+        <label style={{
+          fontSize: "2.75rem",
+          textAlign: "center",
+          marginBottom: "5px",
+          fontWeight: "700"
+        }} htmlFor="title">Todo App</label>
+        <label htmlFor="item">New Item</label>
+        <input
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+          type="text"
+          id="item"
+        />
+      </div>
+      <button className="btn">Add</button>
+    </form>
+  );
 }
